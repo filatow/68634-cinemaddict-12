54983282da22d35f8827e735881f0896c3a7c95c
evgeniy <not-found@inbox.ru>
2020-08-05 19:28:04 +0300

    Выполнил настройку webpack, eslint, gitignore, webpack-dev-server

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-"use strict";
+
 
 const EXTRAFILMLIST_COUNTER = 2;
 const EXTRAFILMLIST_FILM_COUNTER = 2;
@@ -6,16 +6,16 @@ const FILMLIST_FILM_COUNTER = 5;
 
 
 const createUserRankTemplate = () => {
-  return(
+  return (
     `<section class="header__profile profile">
     <p class="profile__rating">Movie Buff</p>
     <img class="profile__avatar" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
   </section>`
   );
-}
+};
 
 const createSiteMenuTemplate = () => {
-  return(
+  return (
     `<nav class="main-navigation">
     <div class="main-navigation__items">
       <a href="#all" class="main-navigation__item main-navigation__item--active">All movies</a>
@@ -26,52 +26,52 @@ const createSiteMenuTemplate = () => {
     <a href="#stats" class="main-navigation__additional">Stats</a>
   </nav>`
   );
-}
+};
 
 const createSortingTemplate = () => {
-  return(
+  return (
     `<ul class="sort">
     <li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
     <li><a href="#" class="sort__button">Sort by date</a></li>
     <li><a href="#" class="sort__button">Sort by rating</a></li>
   </ul>`
   );
-}
+};
 
 const createFilmsTemplate = () => {
-  return(
+  return (
     `<section class="films"></section>`
   );
-}
+};
 
 const createFilmsListTemplate = () => {
-  return(
+  return (
     `<section class="films-list">
       <h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>
-      <div class="films-list__container">       
+      <div class="films-list__container">
       </div>
     </section>`
   );
-}
+};
 
 const createShowMoreButtonTemplate = () => {
-  return(
+  return (
     `<button class="films-list__show-more">Show more</button>`
   );
-}
+};
 
 const createFilmsListExtraTemplate = () => {
-  return(
+  return (
     `<section class="films-list--extra">
     <h2 class="films-list__title">Top rated</h2>
     <div class="films-list__container">
     </div>
   </section>`
   );
-}
+};
 
 const createFilmCardTemplate = () => {
-  return(
+  return (
     `<article class="film-card">
     <h3 class="film-card__title">The Great Flamarion</h3>
     <p class="film-card__rating">8.9</p>
@@ -90,10 +90,10 @@ const createFilmCardTemplate = () => {
     </form>
   </article>`
   );
-}
+};
 
 const createFilmDetailsTemplate = () => {
-  return(
+  return (
     `<section class="film-details">
     <form class="film-details__inner" action="" method="get">
       <div class="form-details__top-container">
@@ -103,22 +103,22 @@ const createFilmDetailsTemplate = () => {
         <div class="film-details__info-wrap">
           <div class="film-details__poster">
             <img class="film-details__poster-img" src="./images/posters/the-great-flamarion.jpg" alt="">
-  
+
             <p class="film-details__age">18+</p>
           </div>
-  
+
           <div class="film-details__info">
             <div class="film-details__info-head">
               <div class="film-details__title-wrap">
                 <h3 class="film-details__title">The Great Flamarion</h3>
                 <p class="film-details__title-original">Original: The Great Flamarion</p>
               </div>
-  
+
               <div class="film-details__rating">
                 <p class="film-details__total-rating">8.9</p>
               </div>
             </div>
-  
+
             <table class="film-details__table">
               <tr class="film-details__row">
                 <td class="film-details__term">Director</td>
@@ -152,29 +152,29 @@ const createFilmDetailsTemplate = () => {
                   <span class="film-details__genre">Mystery</span></td>
               </tr>
             </table>
-  
+
             <p class="film-details__film-description">
               The film opens following a murder at a cabaret in Mexico City in 1936, and then presents the events leading up to it in flashback. The Great Flamarion (Erich von Stroheim) is an arrogant, friendless, and misogynous marksman who displays his trick gunshot act in the vaudeville circuit. His show features a beautiful assistant, Connie (Mary Beth Hughes) and her drunken husband Al (Dan Duryea), Flamarion's other assistant. Flamarion falls in love with Connie, the movie's femme fatale, and is soon manipulated by her into killing her no good husband during one of their acts.
             </p>
           </div>
         </div>
-  
+
         <section class="film-details__controls">
           <input type="checkbox" class="film-details__control-input visually-hidden" id="watchlist" name="watchlist">
           <label for="watchlist" class="film-details__control-label film-details__control-label--watchlist">Add to watchlist</label>
-  
+
           <input type="checkbox" class="film-details__control-input visually-hidden" id="watched" name="watched">
           <label for="watched" class="film-details__control-label film-details__control-label--watched">Already watched</label>
-  
+
           <input type="checkbox" class="film-details__control-input visually-hidden" id="favorite" name="favorite">
           <label for="favorite" class="film-details__control-label film-details__control-label--favorite">Add to favorites</label>
         </section>
       </div>
-  
+
       <div class="form-details__bottom-container">
         <section class="film-details__comments-wrap">
           <h3 class="film-details__comments-title">Comments <span class="film-details__comments-count">4</span></h3>
-  
+
           <ul class="film-details__comments-list">
             <li class="film-details__comment">
               <span class="film-details__comment-emoji">
@@ -229,30 +229,30 @@ const createFilmDetailsTemplate = () => {
               </div>
             </li>
           </ul>
-  
+
           <div class="film-details__new-comment">
             <div for="add-emoji" class="film-details__add-emoji-label"></div>
-  
+
             <label class="film-details__comment-label">
               <textarea class="film-details__comment-input" placeholder="Select reaction below and write comment here" name="comment"></textarea>
             </label>
-  
+
             <div class="film-details__emoji-list">
               <input class="film-details__emoji-item visually-hidden" name="comment-emoji" type="radio" id="emoji-smile" value="smile">
               <label class="film-details__emoji-label" for="emoji-smile">
                 <img src="./images/emoji/smile.png" width="30" height="30" alt="emoji">
               </label>
-  
+
               <input class="film-details__emoji-item visually-hidden" name="comment-emoji" type="radio" id="emoji-sleeping" value="sleeping">
               <label class="film-details__emoji-label" for="emoji-sleeping">
                 <img src="./images/emoji/sleeping.png" width="30" height="30" alt="emoji">
               </label>
-  
+
               <input class="film-details__emoji-item visually-hidden" name="comment-emoji" type="radio" id="emoji-puke" value="puke">
               <label class="film-details__emoji-label" for="emoji-puke">
                 <img src="./images/emoji/puke.png" width="30" height="30" alt="emoji">
               </label>
-  
+
               <input class="film-details__emoji-item visually-hidden" name="comment-emoji" type="radio" id="emoji-angry" value="angry">
               <label class="film-details__emoji-label" for="emoji-angry">
                 <img src="./images/emoji/angry.png" width="30" height="30" alt="emoji">
@@ -264,17 +264,17 @@ const createFilmDetailsTemplate = () => {
     </form>
   </section>`
   );
-}
+};
 
 const createFilmsAmountTemplate = () => {
-  return(
+  return (
     `<p>130 291 movies inside</p>`
   );
-}
+};
 
 const render = (container, template, place) => {
-  container.insertAdjacentHTML(place,template);
-}
+  container.insertAdjacentHTML(place, template);
+};
 
 const siteHeaderElement = document.querySelector(`.header`);
 render(siteHeaderElement, createUserRankTemplate(), `beforeend`);
@@ -288,7 +288,7 @@ const filmsElement = siteMainElement.querySelector(`.films`);
 
 for (let i = 0; i < EXTRAFILMLIST_COUNTER; i++) {
   render(filmsElement, createFilmsListExtraTemplate(), `afterbegin`);
-  const filmsListExtra = filmsElement.querySelector(`.films-list--extra`)
+  const filmsListExtra = filmsElement.querySelector(`.films-list--extra`);
   const filmsListExtraContainer = filmsListExtra.querySelector(`.films-list__container`);
   for (let j = 0; j < EXTRAFILMLIST_FILM_COUNTER; j++) {
     render(filmsListExtraContainer, createFilmCardTemplate(), `beforeend`);
@@ -296,13 +296,16 @@ for (let i = 0; i < EXTRAFILMLIST_COUNTER; i++) {
 }
 
 render(filmsElement, createFilmsListTemplate(), `afterbegin`);
-const filmsList = filmsElement.querySelector(`.films-list`)
+
+const filmsList = filmsElement.querySelector(`.films-list`);
+render(filmsList, createShowMoreButtonTemplate(), `beforeend`);
+
 const filmsListContainer = filmsList.querySelector(`.films-list__container`);
 for (let i = 0; i < FILMLIST_FILM_COUNTER; i++) {
   render(filmsListContainer, createFilmCardTemplate(), `beforeend`);
 }
 
 const siteFooterElement = document.querySelector(`.footer`);
-const footerStatisticsElement = siteFooterElement.querySelector('.footer__statistics');
+const footerStatisticsElement = siteFooterElement.querySelector(`.footer__statistics`);
 render(footerStatisticsElement, createFilmsAmountTemplate(), `beforeend`);
 render(siteFooterElement, createFilmDetailsTemplate(), `afterend`);
