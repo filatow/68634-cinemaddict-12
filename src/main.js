516ae9850a41fc0aabe695418b93cb13c77db037
evgeniy <not-found@inbox.ru>
2020-08-20 03:48:04 +0300

    Реализованы показ и скрытие попапа с подробной информацией о фильме

--- src/main.js
+++ src/main.js
@@ -27,6 +27,35 @@ render(siteHeaderElement, new UserRankView().element, RenderPosition.BEFOREEND);
 
 const siteMainElement = document.querySelector(`.main`);
 
+const renderFilmCard = (filmListContainer, film) => {
+  const filmCardComponent = new FilmCardView(film);
+  const filmDetailsComponent = new FilmsDetailsView(film);
+
+  const showFilmDetailsPopup = () => {
+    siteFooterElement.appendChild(filmDetailsComponent.element);
+  };
+
+  const hideFilmDetailsPopup = () => {
+    siteFooterElement.removeChild(filmDetailsComponent.element);
+  };
+
+  filmCardComponent.element.querySelectorAll(`.film-card__poster, .film-card__title, .film-card__comments`)
+  .forEach((element) => {
+    element.addEventListener(`click`, (event) => {
+      if (element.tagName === `a`) {
+        event.preventDefault();
+      }
+      showFilmDetailsPopup();
+    });
+  });
+
+  filmDetailsComponent.element.querySelector(`.film-details__close-btn`).addEventListener(`click`, () => {
+    hideFilmDetailsPopup();
+  });
+
+  render(filmListContainer, filmCardComponent.element, RenderPosition.BEFOREEND);
+};
+
 render(siteMainElement, new SiteMenuView(filters).element, RenderPosition.BEFOREEND);
 render(siteMainElement, new SortingView().element, RenderPosition.BEFOREEND);
 const filmsSectionComponent = new FilmsSectionView();
@@ -37,7 +66,7 @@ render(filmsSectionComponent.element, filmsListMostCommentedComponent.element, R
 let FilmsListContainerComponent = new FilmsListContainerView();
 render(filmsListMostCommentedComponent.element, FilmsListContainerComponent.element, RenderPosition.BEFOREEND);
 for (let i = 0; i < enumerate.EXTRAFILMLIST_FILM_COUNTER; i++) {
-  render(FilmsListContainerComponent.element, new FilmCardView(filmsMostCommented[i]).element, RenderPosition.BEFOREEND);
+  renderFilmCard(FilmsListContainerComponent.element, filmsMostCommented[i]);
 }
 
 const FilmsListTopRatedComponent = new FilmsListTopRatedView();
@@ -45,7 +74,7 @@ render(filmsSectionComponent.element, FilmsListTopRatedComponent.element, Render
 FilmsListContainerComponent = new FilmsListContainerView();
 render(FilmsListTopRatedComponent.element, FilmsListContainerComponent.element, RenderPosition.BEFOREEND);
 for (let i = 0; i < enumerate.EXTRAFILMLIST_FILM_COUNTER; i++) {
-  render(FilmsListContainerComponent.element, new FilmCardView(filmsTopRated[i]).element, RenderPosition.BEFOREEND);
+  renderFilmCard(FilmsListContainerComponent.element, filmsTopRated[i]);
 }
 
 const FilmsListComponent = new FilmsListView();
@@ -53,7 +82,7 @@ render(filmsSectionComponent.element, FilmsListComponent.element, RenderPosition
 FilmsListContainerComponent = new FilmsListContainerView();
 render(FilmsListComponent.element, FilmsListContainerComponent.element, RenderPosition.AFTERBEGIN);
 for (let i = 0; i < Math.min(enumerate.FILM_COUNT_PER_STEP, films.length); i++) {
-  render(FilmsListContainerComponent.element, new FilmCardView(films[i]).element, RenderPosition.BEFOREEND);
+  renderFilmCard(FilmsListContainerComponent.element, films[i]);
 }
 
 
@@ -68,7 +97,7 @@ if (films.length > enumerate.FILM_COUNT_PER_STEP) {
 
     films
       .slice(renderedFilmCount, renderedFilmCount + enumerate.FILM_COUNT_PER_STEP)
-      .forEach((film) => render(FilmsListContainerComponent.element, new FilmCardView(film).element, RenderPosition.BEFOREEND));
+      .forEach((film) => renderFilmCard(FilmsListContainerComponent.element, film));
 
     renderedFilmCount += enumerate.FILM_COUNT_PER_STEP;
     if (renderedFilmCount > films.length) {
@@ -82,4 +111,3 @@ if (films.length > enumerate.FILM_COUNT_PER_STEP) {
 const siteFooterElement = document.querySelector(`.footer`);
 const footerStatisticsElement = siteFooterElement.querySelector(`.footer__statistics`);
 render(footerStatisticsElement, new FilmsAmountView().element, RenderPosition.BEFOREEND);
-render(siteFooterElement, new FilmsDetailsView(films[0]).element, RenderPosition.AFTERBEGIN);
